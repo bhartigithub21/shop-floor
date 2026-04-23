ba4aec91013b991389fe5d4d98dfbfd1912050d8
Bapiya <bapiya1@gmail.com>
2026-04-23 15:04:18 +0530

    context

---
+++ frontend/src/config/AppContext.jsx
@@ -0,0 +1,14 @@
+import React, { createContext, useState } from "react";
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const value = {
+    user,
+    setUser,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
