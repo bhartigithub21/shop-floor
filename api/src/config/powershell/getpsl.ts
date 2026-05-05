1dbbd0f83393c28b757d703d79c4e9ab8eb8b243
Bapiya <bapiya1@gmail.com>
2026-05-05 17:19:43 +0530

    date fix

--- api/src/config/powershell/getpsl.ts
+++ api/src/config/powershell/getpsl.ts
@@ -1,18 +1,24 @@
 import { exec } from "child_process";
 
 export const getData = (): Promise<string> => {
-  const date = new Date(Date.now() - 48 * 60 * 60 * 1000).getDate();
-  console.log(date);
+  const date = new Date(Date.now() - 48 * 60 * 60 * 1000);
+  const srchDt = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+  console.log(srchDt);
 
-  return new Promise((resolve, reject) => {
-    const cmd = `
-    Invoke-RestMethod -Uri "http://172.16.8.211:7258/EIPPL/api/bapiya/demo/v1.0/companies(a1f36400-efd6-ef11-8292-00155d010600)/psls?\$filter=ScheduleDate%20ge%202026-04-27&\$top=70" -Method Get -UseDefaultCredentials |
-    ConvertTo-Json -Depth 10
-    `;
+  return new Promise<string>((resolve, reject) => {
+    const cmd = `powershell -NoProfile -Command "(Invoke-WebRequest -Uri 'http://172.16.8.211:7258/EIPPL/api/bapiya/demo/v1.0/companies(a1f36400-efd6-ef11-8292-00155d010600)/psls?\$filter=ScheduleDate%20ge%202026-04-27&\$top=100' -UseDefaultCredentials -UseBasicParsing).Content"`;
+    exec(cmd, { maxBuffer: 10 * 1024 * 1024 }, (err, stdout, stderr) => {
+      if (err) {
+        console.error("err:", err);
+        console.error("stderr:", stderr);
+        return reject(stderr || err.message);
+      }
 
-    exec(`powershell -Command "${cmd}"`, (err, stdout, stderr) => {
-      if (err) return reject(stderr);
-      resolve(stdout as string);
+      if (!stdout) {
+        return reject("Empty stdout");
+      }
+
+      resolve(stdout.trim());
     });
   });
 };
