52a1fff7cfc3af9bd4bf6d8afceac4e5d9be3863
Bapiya <bapiya1@gmail.com>
2026-05-06 15:09:52 +0530

    bac update

--- api/src/config/powershell/test.ts
+++ api/src/config/powershell/test.ts
@@ -1,12 +1,74 @@
 import { exec } from "child_process";
 
-export const postData = (
-  no: String,
-  output: number,
-  user: String,
-): Promise<string> => {
+export const postData = (data: {
+  no: string;
+  docNo: string;
+  lineNo: number;
+  userName: string;
+  startDate: string;
+  endDate: string;
+  startTime: string;
+  endTime: string;
+  scrapCode: string;
+  scrapQnt: number;
+  downDate: string;
+  downstartTime: string;
+  downEndTime: string;
+  downReason: string;
+  setupTime: number;
+  output: number;
+  user: string;
+}): Promise<string> => {
+  const {
+    no,
+    docNo,
+    lineNo,
+    userName,
+    startDate,
+    endDate,
+    startTime,
+    endTime,
+    scrapCode,
+    scrapQnt,
+    downDate,
+    downstartTime,
+    downEndTime,
+    downReason,
+    setupTime,
+    output,
+    user,
+  } = data;
+
   return new Promise<string>((resolve, reject) => {
-    const cmd = `powershell -NoProfile -Command "$body = @{ No = '${no}'; Output = ${output}; User = '${user}' } | ConvertTo-Json -Depth 10; (Invoke-RestMethod -Uri 'http://172.16.8.211:7258/EIPPL/api/bapiya/demo/v1.0/companies(a1f36400-efd6-ef11-8292-00155d010600)/tests' -Method Post -Body $body -ContentType 'application/json' -UseDefaultCredentials) | ConvertTo-Json -Depth 10"`;
+    const cmd = `powershell -NoProfile -Command "
+    $body = @{
+      No = '${no}';
+      DocNo = '${docNo}';
+      lineNo = ${lineNo};
+      userName = '${userName}';
+      startDate = '${startDate}';
+      endDate = '${endDate}';
+      startTime = '${startTime}';
+      endTime = '${endTime}';
+      scrapCode = '${scrapCode}';
+      scrapQnt = ${scrapQnt};
+      downDate = '${downDate}';
+      downstartTime = '${downstartTime}';
+      downEndTime = '${downEndTime}';
+      downReason = '${downReason}';
+      setupTime = ${setupTime};
+      Output = ${output};
+      User = '${user}'
+    } | ConvertTo-Json -Depth 10;
+
+    Invoke-RestMethod 
+      -Uri 'http://172.16.8.211:7258/EIPPL/api/bapiya/demo/v1.0/companies(a1f36400-efd6-ef11-8292-00155d010600)/tests' 
+      -Method Post 
+      -Body $body 
+      -ContentType 'application/json' 
+      -UseDefaultCredentials
+    | ConvertTo-Json -Depth 10
+    "`;
 
     exec(cmd, { maxBuffer: 10 * 1024 * 1024 }, (err, stdout, stderr) => {
       if (err) {
--- api/src/routes/dummy.ts
+++ api/src/routes/dummy.ts
@@ -7,7 +7,25 @@ router.get("/psl", (req, res) => {
   res.json(datJson.value);
 });
 router.get("/test", async (req, res) => {
-  const result = await postData("abc123", 20, "ripr");
+  const result = await postData({
+    no: "123",
+    docNo: "456",
+    lineNo: 1,
+    userName: "John Doe",
+    startDate: "2024-01-01",
+    endDate: "2024-01-02",
+    startTime: "09:00:00",
+    endTime: "17:00:00",
+    scrapCode: "SC123",
+    scrapQnt: 10,
+    downDate: "2024-01-01",
+    downstartTime: "10:00:00",
+    downEndTime: "11:00:00",
+    downReason: "Machine Maintenance",
+    setupTime: 30,
+    output: 100,
+    user: "JDoe",
+  });
   res.json(result);
 });
 
