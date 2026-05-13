235517d2e8d6cb7c902d867a25dc8b3d4ecff307
Bapiya <bapiya1@gmail.com>
2026-05-13 09:40:09 +0530

    api remove dummy

--- api/src/routes/dummy.ts
+++ api/src/routes/dummy.ts
@@ -7,25 +7,8 @@ router.get("/psl", (req, res) => {
   res.json(datJson.value);
 });
 router.get("/test", async (req, res) => {
-  const result = await postData({
-    no: "123",
-    docNo: "456",
-    lineNo: "1",
-    userName: "John Doe",
-    startDate: "2024-01-01",
-    endDate: "2024-01-02",
-    startTime: "09:00:00",
-    endTime: "17:00:00",
-    scrapCode: "SC123",
-    scrapQnt: "10",
-    downDate: "2024-01-01",
-    downstartTime: "10:00:00",
-    downEndTime: "11:00:00",
-    downReason: "Machine Maintenance",
-    setupTime: "30",
-    output: "100",
-    user: "JDoe",
-  });
+  const { data } = req.body;
+  const result = await postData(data);
   res.json(result);
 });
 
